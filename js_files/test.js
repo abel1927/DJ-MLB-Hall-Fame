fd834836259d575c2266ef8a2ee63a86f536a7fe
Belzico <61699417+Belzico@users.noreply.github.com>
2022-07-13 23:35:58 -0400

    steroids updated

--- js_files/test.js
+++ js_files/test.js
@@ -1,14 +1,41 @@
 
 console.log('Correct')
-
-document.querySelector('#-BUTTON-BABE-').addEventListener('click', traerBabe)
-
-document.querySelector('#-BUTTON-PLAYER-').addEventListener('click', traerPlayer)
-
 var defaultPlayerValue=document.querySelector('#-PLAYER-SHOW-BODY1-').innerHTML;
+
+//document.querySelector('#-BUTTON-NEGRO-').addEventListener('click', negroLeague);
+
+document.querySelector('#-BUTTON-BABE-').addEventListener('click', traerBabe);
+
+document.querySelector('#-BUTTON-PLAYER-').addEventListener('click', traerPlayer);
+
+
+
+
 console.log(defaultPlayerValue)
 
-function NegroLeague(){
+function showSteroidChance(){
+
+    console.log('Dentro de la funcion de traer steroid chance');
+
+    const xhttp =new XMLHttpRequest();
+
+    xhttp.open('GET','../Data/mlb_players.json',true);
+
+    xhttp.send();
+    xhttp.onreadystatechange = function(){
+        if(this.readyState==4 && this.status==200){
+            let steroidChance= Math.random();
+            console.log(steroidChance)
+            window.alert(steroidChance*100 +"%" );
+
+            }
+    }
+
+}
+
+
+
+function negroLeague(){
     console.log('Dentro de la funcion de traer negro league');
 
     const xhttp =new XMLHttpRequest();
@@ -17,8 +44,109 @@ function NegroLeague(){
 
     xhttp.send();
     xhttp.onreadystatechange = function(){
+        if(this.readyState==4 && this.status==200){
+            let nList=["George Louis Scales","James William Gilliam","Elston Gene Howard","Allen Hurley McNair","Alphonse Eugene Smith","Newton Henry Allen","Albert Dewey Creacy","Branch Lee Russell","Henry Curtis Thompson","Clinton Cyrus Thomas","John Christopher Beckwith","Richard Benjamin Lundy","Walter Moore","Edgar Wooded Wesley","William Bell","Samuel Jones","Donald Newcombe","Barney Brown","Joseph Black","Clifford Bell","William McKinley Cornelius","Philip Cockrell","Nelson Dean","William Force"];
+            window.alert(nList);
+
+            }
+    }
 
 }
+
+function traerSteroid(){
+    console.log('Dentro de la funcion de traer steroid');
+
+    const xhttp =new XMLHttpRequest();
+
+    xhttp.open('GET','../Data/mlb_players.json',true);
+
+    xhttp.send();
+    xhttp.onreadystatechange = function(){
+        if(this.readyState==4 && this.status==200){
+
+            let datos = JSON.parse(this.responseText); 
+
+            console.log('Carge los datos')
+            
+            let playerName=document.querySelector('#-PLAYER-NAME-STEROID-');
+            
+            console.log(playerName.value)
+
+            playerName=playerName.value;
+            playerId='';
+
+            for(item in datos){
+                //console.log(datos[item]["Name"]);
+                if (datos[item]["Name"]==playerName)
+                {
+                    playerId=item;
+                    console.log("succes");
+                    break;
+                    
+                }
+            }
+            if (playerId!=''){
+                console.log("entre al if");
+
+                let playerRes=document.querySelector('#-STEROID-SHOW-BODY1-');
+                playerRes.innerHTML='';
+            
+            if ("G" in datos[playerId]["batter_stats"]){
+                let temp_text=`<tr>
+            <td>${datos[playerId]["First year"]}</td>
+            <td>${datos[playerId]["Last year"]}</td>
+            <td>${datos[playerId]["batter_stats"]["G"]["summary"]}</td>
+            <td>${datos[playerId]["batter_stats"]["HR"]["summary"]}</td>
+            <td>${datos[playerId]["batter_stats"]["BA"]["summary"]}</td>
+            <td>${datos[playerId]["batter_stats"]["RBI"]["summary"]}</td>
+            <td>${datos[playerId]["batter_stats"]["BB"]["summary"]}</td>
+            <td>${datos[playerId]["batter_stats"]["OBP"]["summary"]}</td>
+            <td>${datos[playerId]["batter_stats"]["SLG"]["summary"]}</td>
+            </tr>`;
+                playerRes.innerHTML+=temp_text;
+                console.log(  playerRes.innerHTML +"lolol");
+            }
+            else
+            {
+                let playerRes=document.querySelector('#-STEROID-SHOW-BODY1-');
+                playerRes.innerHTML=defaultPlayerValue;
+            } 
+        
+            
+            if ("ERA" in datos[playerId]["pitcher_stats"]){
+            temp_text=`<tr>
+            <td>${datos[playerId]["First year"]}</td>
+            <td>${datos[playerId]["Last year"]}</td>
+            <td>${datos[playerId]["pitcher_stats"]["G"]["summary"]}</td>
+            <td>${datos[playerId]["pitcher_stats"]["W"]["summary"]}</td>
+            <td>${datos[playerId]["pitcher_stats"]["L"]["summary"]}</td>
+            <td>${datos[playerId]["pitcher_stats"]["ERA"]["summary"]}</td>
+            <td>${datos[playerId]["pitcher_stats"]["IP"]["summary"]}</td>
+            <td>${datos[playerId]["pitcher_stats"]["H"]["summary"]}</td>
+            <td>${datos[playerId]["pitcher_stats"]["SO"]["summary"]}</td>
+            </tr>`;
+
+            let playerResPit=document.querySelector('#-STEROID-SHOW-BODY2-');
+            playerResPit.innerHTML=temp_text;
+            console.log(  playerRes.innerHTML +"lolol");
+            
+        }
+        else
+        {
+            let playerRes=document.querySelector('#-PLAYER-SHOW-BODY2-');
+            playerRes.innerHTML=defaultPlayerValue;
+        } 
+            }
+            else{
+                window.alert("No existe ese jugador en nuestra base de datos de steriodes.");
+            }
+            
+
+            }
+    }
+
+}
+
 
 function traerPlayer(){
     console.log('Dentro de la funcion de traer player');
